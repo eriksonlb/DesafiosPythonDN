8120726942a28ffd954fa229407c49b6460434e7
erikson.bezerra <erikson.bezerra@dieboldnixdorf.com.br>
2019-09-06 17:03:34 -0300

    Implementei o Try Except e usei a função repeat quando o calculo for feito com string

--- Multiplica.py
+++ Multiplica.py
@@ -1,12 +1,21 @@
+"esta dando erro porque os dados estão vindo como string. thing about it"
 def multiplica(a, b):
-    resultado = 0
-    for n in range(a):
-        resultado += b
-    return resultado
+    resultado = None
+    if type(a) == str or type(b) == str:
+        if type(a) == str:
+            for n in range(b):
+                resultado += a
+            return resultado
+        else:
+            for n in range(a):
+                resultado += b
+            return resultado
+    else:
+        return a / (1 / b)
 
 
-valor1 = int(input('Digite o multiplicador: '))
-valor2 = int(input('Digite o multiplicando: '))
+valor1 = input('Digite o multiplicador: ')
+valor2 = input('Digite o multiplicando: ')
 result = multiplica(valor1, valor2)
 print(f'{valor1} x {valor2} = {result}')
 
--- Multiplica2.py
+++ Multiplica2.py
@@ -1,7 +1,37 @@
-def multiplica_sem_loop(a,b)
-    return a / (1 / b)
+from itertools import repeat
 
-    valor1 = int(input('Digite o multiplicador: '))
-    valor2 = int(input('Digite o multiplicando: '))
-    result = multiplica_sem_loop(valor1, valor2)
+
+def multiplica(a, b):
+    try:
+        floatValue1 = float(a); floatValue2 = float(b)        
+        return floatValue1 / (1 / floatValue2)
+    except ValueError:
+        try:
+            float(a)
+            repeteString = str(list(repeat(b, int(a))))
+            return removeCaractere(repeteString)
+        except ValueError:
+            float(b)
+            repeteString = str(list(repeat(a, int(b))))
+            return removeCaractere(repeteString)
+
+
+def aplicacao():
+    valor1 = input('Digite o multiplicador: ')
+    valor2 = input('Digite o multiplicando: ')
+    result = multiplica(valor1, valor2)
     print(f'{valor1} x {valor2} = {result}')
+
+
+def removeCaractere(textoCorrigido):
+    caractereEspecial1 = str.replace(textoCorrigido, '[', '')
+    caractereEspecial2 = str.replace(caractereEspecial1, ']', '')
+    caractereEspecial3 = str.replace(caractereEspecial2, ',', '')
+    corrigido = str.replace(caractereEspecial3, "'", "")
+    return corrigido
+
+aplicacao()
+
+
+
+
